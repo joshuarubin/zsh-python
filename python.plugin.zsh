bcc9d31c5f6985d52fc8a041a47f5918cd8c9fba
Joshua Rubin <jrubin@zvelo.com>
2014-10-29 12:15:28 -0600

    add pyenv
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- python.plugin.zsh
+++ python.plugin.zsh
@@ -1,8 +1,47 @@
+#
+# Enables local Python package installation.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#   Sebastian Wiesner <lunaryorn@googlemail.com>
+#
+
+if (( $+commands[pyenv] )); then
+  #eval "$(pyenv init -)"
+  export PYENV_SHELL=zsh
+  source '/usr/local/Cellar/pyenv/20141012/completions/pyenv.zsh'
+  #pyenv rehash 2>/dev/null
+  pyenv() {
+    local command
+    command="$1"
+    if [ "$#" -gt 0 ]; then
+      shift
+    fi
+
+    case "$command" in
+    rehash|shell)
+      eval "`pyenv "sh-$command" "$@"`";;
+    *)
+      command pyenv "$command" "$@";;
+    esac
+  }
+fi
+
+# Return if requirements are not found.
+if (( ! $+commands[python] && ! $+commands[pyenv] )); then
+  return 1
+fi
+
 if (( $+commands[virtualenvwrapper_lazy.sh] )); then
   export WORKON_HOME=$HOME/.virtualenvs
   export PROJECT_HOME=$HOME/working
   export VIRTUALENVWRAPPER_SCRIPT=$(which virtualenvwrapper.sh)
+  VIRTUAL_ENV_DISABLE_PROMPT=1
   source $(which virtualenvwrapper_lazy.sh)
 fi
 
-VIRTUAL_ENV_DISABLE_PROMPT=true
+#
+# Aliases
+#
+
+alias py='python'
